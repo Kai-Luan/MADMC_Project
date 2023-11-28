aaaffc315de337e8fcfd45ead940d72129973ef8
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2023-11-28 17:13:45 +0100

    ND_Tree finished

--- Code/test.py
+++ Code/test.py
@@ -1,3 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
 
-xStart=np.zeros(200,dtype=int) # n solutions
+n= 100
+data = np.random.randint(100, size=(n,2))
+
+plt.scatter(*data.T)
+plt.show()
+
+
+
+
