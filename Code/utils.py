4a738274724f18e3a026f85dd5910162ed58a007
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2023-12-01 16:48:59 +0100

    Merge branch 'main' of https://github.com/Kai-Luan/MADMC_Project

--- Code/utils.py
+++ Code/utils.py
@@ -4,148 +4,52 @@ from indicators import *
 from gurobipy import GRB
 import gurobipy as gp
 
-def miseAJour(X, x):
-	if X:
-		p = len(X[0])	
-		for y in X:
-			b = True
-			for i in range(p):
-				if y[1][i] >= x[1][i]: pass
-				else:
-					b = False
-					break		
-			if b: return False
-		X.append(x)
-		i = 0
-		n = len(X)
-		while i < n:
-			y = X[i]
-			b = True
-			for j in range(p):
-				if x[1][j] > y[1][j]: pass
-				else:
-					b = False
-					break	
-			if b: 
-				X.pop(i)
-				n -= 1
-			else: i+=1
-		return True	
-	else:
-		X.append(x)
-		return True
-
-def discrete_binary_search(tab, lo, hi, x):
-	while lo < hi:
-		mid = lo + (hi - lo) // 2
-		if x[1][0] <= tab[mid][1][0]:
-			hi = mid
-		else:
-			lo = mid + 1
-	return lo
-
-# On suppose X triés croissant, et bi-ojectif
-def miseAJour2(X, x):
-	if X:
-		p = len(X[0])	
-		index = discrete_binary_search(X, 0, len(X), x)
-		for y in X[index:]:
-			# y domine faiblement x
-			if y[1][1] >= x[1][1]:
-				return False
-		# print("INDEX: ", index, len(X), x[1])
-		# print("X ",[y[1] for y in X])
-		X.insert(index, x)
-		# print("X ",[y[1] for y in X])
-		i = 0
-		while i < index:
-			y = X[i]
-			if x[1][1] > y[1][1]:
-				# print("POP: ", x[1], y[1])
-				X.pop(i)
-				index -= 1
-			else: i+=1
-		return True	
-	else:
-		X.append(x)
-		return True
-
-
-def init(m, n, p, v, w, W):
-	"""
-	m: le nombre de solution à générer
-	n: dimension de la solution
-	p: le nombre de critères
-	v: utilité des n objets
-	w: poids des n objets
-	W: la taille du sac
-	"""
-	res = []
-	def rapport(v1, v2, poids, q):
-		return (q*v1 +(1-q)*v2)/poids
-	
-	for _ in range(m):
-		q = np.random.random()
-		xStart=np.zeros(n,dtype=int) # n solutions
-		arr = [rapport(v[j][0], v[j][1], w[j], q) for j in range(n)]
-		arr = np.argsort(arr)[::-1]
-		wTotal=0
-		vStart=np.zeros(p,dtype=int) # objectifs
-		for i in range(n):
-			if wTotal+w[arr[i]]<=W:
-				xStart[arr[i]]=1
-				wTotal=wTotal+w[arr[i]]
-				vStart+=v[arr[i]]
-		res.append([xStart, vStart])
-	return res
 
 # Initialisation generalisee
-
 # generalized version, it takes a vector allv and a vector q
-def rapport_gen(allv, poids, q):
+def rapport(allv, poids, q):
 	# ([v1, ..., vn] * [q1, ..., qn])/poids
 	return np.sum((q*allv))/poids
 
 # Initialisation de n solutions
 # m : solutions aléatoires, w : vecteur poids, W : taille du sac, v : vecteur valeurs, n : nombre d'objets, p : nombre d'objectifs.
-def init_gen(m,w,W,v,n,p):
+def init(m,params):
+	(n,p,v,w,W) = params
 	res = []
 	for _ in range(m):
-		#q = np.random.random()
 		# generalized q (instead of bi-distrib)
 		q = np.random.dirichlet(np.ones(6),size=1)[0]
 		xStart=np.zeros(200,dtype=int) # n solutions
 		# on range dans l'ordre decroissant
-		#arr = [rapport(v[j][0], v[j][1], w[j], q) for j in range(n)]
 		arr = [rapport(v[j], w[j], q) for j in range(n)]
 		arr = np.argsort(arr)[::-1]
 
 		# total mass of the bag
 		wTotal=0
-
 		# initial objective values
 		vStart=np.zeros(p,dtype=int) # objectifs
 
 		# we loop on all the items
-		for i in range(n):
+		for i in arr:
 			# On prend les objets selon le rapport jusqu'à ce qu'il n'y a plus de place
-			if wTotal+w[arr[i]]<=W:
-				xStart[arr[i]]=1
-				wTotal=wTotal+w[arr[i]]
-				for j in range(p):
-					vStart[j]=vStart[j]+v[arr[i],j]
+			if wTotal+w[i]<=W:
+				xStart[i]=1
+				wTotal+=w[i]
+				vStart+=v[i]
 		res.append([xStart, vStart])
 	return res
 
 # Fonction de voisinage
-def voisinage(x, n, v, w, W):
+def voisinage(x, params):
 	"""
 	x: solution à voisiner
-	n: dimension de la solution
-	v: utilité des n objets
-	w: poids des n objets
-	W: la taille du sac
+	params: (n,_,v,w,W)
+		n: dimension de la solution,
+		v: utilité des n objets,
+		w: poids des n objets,
+		W: la taille du sac
 	"""
+	(n,_,v,w,W) = params
 	res = []
 	poids = sum(w*x[0])
 	for i in range(n):
@@ -160,22 +64,42 @@ def voisinage(x, n, v, w, W):
 			res.append([copyx, copyv])
 	return res
 
+def PLS(m, params, NBMAX= 20, verbose=False):
+	population = init(m,params)
+	Xe = NDTree(NBMAX=NBMAX)
+	for e in population: Xe.update(e)
+	Pa = NDTree(NBMAX=NBMAX)
+
+	iteration = 1
+	while population:
+		if verbose: print(f'{iteration = } | population size: {len(population)}')
+		for p in population:
+			for candidat in voisinage(p, params):
+				if np.all(p[1] >= candidat[1]): continue
+				if Xe.update(candidat): Pa.update(candidat)
+		population = Pa.getPoints()
+		Pa.reset()
+		iteration += 1
+	return Xe.getPoints()
+
 # ======================== ND-Tree ================================
 class Node():
 	def __init__(self, y, pere=None):
-		self.pi = y
-		self.pn = y
+		self.pi = y[1]
+		self.pn = y[1]
 		self.points = [y]
 		self.pere = pere
+		self.count += 1
+		self.toremove = []
 
 	def isLeaf(self):
 		return not isinstance(self.points[0], Node)
 	
 	def isEmpty(self):
-		return len(self.points) == 0
+		return self.points == []
 	
 	def closest(self, y):
-		y = np.array(y)
+		y = np.array(y[1])
 		P = [(x.pi+x.pn)/2 for x in self.points]
 		P = np.array(P)
 		D = np.linalg.norm(P-y, axis=1)
@@ -183,22 +107,34 @@ class Node():
 		return self.points[index]
 	
 	def remove(self, z):
+		self.toremove.append(z)
+
+	def refresh(self):
+		if self.toremove:
+			self.points = [x for x in self.points if x not in self.toremove]
+			self.toremove = []
+
+	def replace(self, z, obj):
 		for i,p in enumerate(self.points):
-			if np.all(z==p):
-				self.points.pop(i)
+			if z is p: 
+				self.points[i] = obj
 				break
 	
+	def append(self, x):
+		self.points.append(x)
+
 	def updateIdealNadir(self, y):
 		b = 0
 		points = []
-		if self.isLeaf(): points = self.points
+		if self.isLeaf(): 
+			points = [p[1] for p in self.points]
 		else:
 			for x in self.points:
 				points.append(x.pi)
 				points.append(x.pn)
 		a = np.max(points, 0)
 		b = np.min(points, 0)
-		if np.any(b < self.pn) or np.any(a > self.pi):
+		if (not np.array_equal(b,self.pn)) or (not np.array_equal(a,self.pi)):
 			self.pi = a
 			self.pn = b
 			if self.pere is not None: self.pere.updateIdealNadir(y)
@@ -207,51 +143,51 @@ class Node():
 		"""
 		return False if y is dominated, else True
 		"""
-		if np.all(self.pn >= y): return False
-		elif np.all(y >= self.pi): 
+		if np.all(self.pn >= y[1]): return False
+		elif np.all(y[1] >= self.pi): 
 			# Remove self and sub_tree of n
 			if self.pere is not None: self.pere.remove(self)
 			return True
-		elif np.all(self.pi >= y) or np.all(y >= self.pn):
+		elif np.all(self.pi >= y[1]) or np.all(y[1] >= self.pn):
 			if self.isLeaf(): 
 				# on supprime les solutions dominées
 				# sinon, le candidat y s'il est dominé
 				L = []
 				for z in self.points:
-					if np.all(z >= y): return False
-					elif not np.all(y > z): L.append(z)
+					if np.all(z[1] >= y[1]): return False
+					elif np.any(z[1] > y[1]): L.append(z)
 				self.points = L
+				self.updateIdealNadir(y)
 			else: 
+				L = []
 				for x in self.points:
-					if not x.updateNode(tree, y): return False
-					elif x.isEmpty(): self.remove(x)
+					if not x.updateNode(tree, y): # x domine y
+						return False
+					elif not x.isEmpty(): L.append(x)
+				self.points = L
 				# supprime les noeuds à un successeur
 				if len(self.points) == 1:
 					node = self.points[0]
-					if self.pere is not None:
-						self.pere.remove(self)
-						self.pere.append(node)
+					if self.pere is not None: self.pere.replace(self,node)
 					else: tree.root = node
+		self.refresh()
 		return True
-
+	
 	def split(self, nChild=2):
-		points = np.array(self.points)
+		points = np.array([p[1] for p in self.points])
 		D = [
 			np.linalg.norm(points - p, axis=1).mean()
 			for p in points
 		]
 		I = np.argsort(D)[-nChild:]
 		I.sort()
-		
-		N, Z = [], []
+		N = []
+		# Create New Nodes
 		for i in I[::-1]:
 			z = self.points.pop(i)
 			N.append(Node(z, pere=self))
-			Z.append(z)
-		Z = np.array(Z)
-		P = self.points
-		self.points = N
-		while P:
+		self.points, P = N, self.points
+		while P: # Assign remaining solutions to leafs
 			z = P.pop()
 			tmp = self.closest(z)
 			tmp.points.append(z)
@@ -274,20 +210,29 @@ class NDTree():
 	# return (YND, Squares)
 	# YND non dominated solutions
 	# Squares: (depth, ideal point, nadir point) of each node
-	def getPoints(self, leafOnly = True):
-		def get(node,count=0):
+	def getPoints(self):
+		def get(node):
 			if node.isLeaf():
-				return node.points, [(count,node.pi, node.pn)]
+				return node.points
 			else:
 				L = []
-				M = [] if leafOnly else [(count,node.pi, node.pn)]
 				for x in node.points: 
-					ynd, yid = get(x, count+1)
-					L.extend(ynd)
-					M.extend(yid)
-				return L, M
+					r = get(x)
+					L.extend(r)
+				return L
 		return get(self.root)
 	
+	def getSquares(self, leafOnly = True):
+		def get(node,count=0):
+			if node.isLeaf(): return [(count,node.pi, node.pn)]
+			else:
+				M = [] if leafOnly else [(count,node.pi, node.pn)]
+				for x in node.points: 
+					yid = get(x, count+1)
+					M.extend(yid)
+				return M
+		return get(self.root)
+
 	def update(self,y):
 		dim = len(y)
 		if self.root is None:
@@ -298,6 +243,8 @@ class NDTree():
 			return True
 		return False
 
+	def reset(self):
+		self.root = None
 # MR: max regret: x in O
 # PMR: pairwise max regret, (x,y) in O
 # MMR: minimax regret: x
